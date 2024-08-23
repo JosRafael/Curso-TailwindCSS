6790a9ce8c66fe64d7cd99a89644c27b9a8c08d1
JosRafael <rafaelmatiashue@gmail.com>
2024-08-23 11:17:56 -0300

    Criando animações no tema

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,13 +1,22 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./src/**/*.{html,js}"],
+  content: ['./src/**/*.{html,js}'],
   theme: {
     extend: {
-      colors:{
-        primaria: "#FF00FF",
-      }
+      keyframes: {
+        floatUp: {
+          '0%': { transform: 'translate(0px)' },
+          '50%': { transform: 'translate(0, -8px)' },
+          '100%': { transform: 'translate(0)' },
+        },
+      },
+      animation: {
+        floatUp: 'floatUp 2s',
+      },
+      colors: {
+        primaria: '#FF00FF',
+      },
     },
   },
   plugins: [],
 }
-
